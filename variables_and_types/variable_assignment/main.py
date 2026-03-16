e72079a15472b828b3bfad3b1977170a301df931
yanghangsu <>
2026-03-16 11:15:47 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,11 @@
+item_name = "Eggs"
+item_price = 3.99
+item_stock_quantity = 120
+in_stock = True
+
+
+# Testing
+print("Item : ", item_name)
+print("Price per unit: $", item_price)
+print("quantity in stock: ", item_stock_quantity)
+print("In stock: ", in_stock)
